67d2dd6d9ff537b55c4cd66bc77c86de6ac43397
aaravsinghtiwari <centuarusff@gmail.com>
2026-06-14 13:42:53 +0530

    release 1.0

---
+++ Script.js
@@ -0,0 +1,6 @@
+const imageElement = document.getElementById('img1');
+
+// Add a click event listener to handle navigation
+imageElement.addEventListener('click', () => { 
+    window.location.href = "Index.html ";
+});
